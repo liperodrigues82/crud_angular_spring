128353cf3ef463e81587eff7ebec8cf6f066da5e
Felipe Rodrigues <68449279+liperodrigues82@users.noreply.github.com>
2022-08-22 10:25:41 -0300

    customizando o tema do angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'courses' }
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
 ];
 
 @NgModule({
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-import { CursosComponent } from './cursos/cursos.component';
 import { CoursesComponent } from './courses/courses.component';
 
 
 @NgModule({
   declarations: [
-    CursosComponent,
     CoursesComponent
   ],
   imports: [
